aa40fdad2c61aab66fb9c6e6bca0e058229720c0
TinaRum <ipochinili@gmail.com>
2021-11-17 01:17:50 +0300

    loc perm

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Loading from './Loading';
+import { Alert } from 'react-native';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-      <Loading/>
-  );
+
+export default class extends React.Component {
+
+  getLocation = async () => {
+    try {
+      const response = await Location.getPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert('Не могу определить местоположение', "Грусть, печаль :(");
+    }
+
+
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return (
+      <Loading />
+    );
+  }
 }
